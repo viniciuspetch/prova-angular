a0461655cac92231f05097893e1dc22ce20e829a
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-05-06 14:56:43 -0300

    Set a better default user list, organized stuff

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -5,48 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class UsersService {
   currUser
-  users = [
-    {
-      name: 'Maria Flores',
-      cpf: '83321492075',
-      phone: '1533283928',
-      email: 'maria_f@gmail.com',
-      cep: '69906043',
-      state: 'AC',
-      city: 'Rio Branco',
-      street: 'Rua Arnaldo Aleixo (Conjunto Canaã)',
-    },
-    {
-      name: 'João Carlos',
-      cpf: '31213393035',
-      phone: '1532841040',
-      email: 'joao.c@gmail.com',
-      cep: '79096766',
-      state: 'MS',
-      city: 'Campo Grande',
-      street: 'Rua Rodolfho José Rospide da Motta',
-    },
-    {
-      name: 'Stephanie Dias',
-      cpf: '02085196020',
-      phone: '1533294040',
-      email: 'ste.dias@gmail.com',
-      cep: '23825080',
-      state: 'RJ',
-      city: 'Itaguaí',
-      street: 'Rua Mario Bastos Filho',
-    },
-    {
-      name: 'Mirtes Souza',
-      cpf: '33764389001',
-      phone: '1530178756',
-      email: 'irma.mirtes@gmail.com',
-      cep: '40420150',
-      state: 'BA',
-      city: 'Salvador',
-      street: '1ª Travessa Clóvis de Almeida Maia',
-    }
-  ]
+  users = []
 
   resetCurrUser() {
     this.currUser = { name: "", cpf: "", phone: "", email: "", cep: "", state: "", city: "", street: "" }
@@ -56,8 +15,62 @@ export class UsersService {
     return this.users.findIndex(listUser => Number(listUser.cpf) == Number(user.cpf))
   }
 
-  updateLocalStorage() {
+  saveLocalStorage() {
     localStorage.setItem("crud_users", JSON.stringify(this.users))
+  }
+
+  loadLocalStorage() {
+    var stg = localStorage.getItem('crud_users')
+    // Local content exists, use it
+    if (stg) {
+      this.users = JSON.parse(stg)
+    } 
+    // no local data found, use default
+    else {
+      this.users = [
+        {
+          name: 'Maria Flores',
+          cpf: '83321492075',
+          phone: '1533283928',
+          email: 'maria_f@gmail.com',
+          cep: '69906043',
+          state: 'AC',
+          city: 'Rio Branco',
+          street: 'Rua Arnaldo Aleixo (Conjunto Canaã)',
+        },
+        {
+          name: 'João Carlos',
+          cpf: '31213393035',
+          phone: '1532841040',
+          email: 'joao.c@gmail.com',
+          cep: '79096766',
+          state: 'MS',
+          city: 'Campo Grande',
+          street: 'Rua Rodolfho José Rospide da Motta',
+        },
+        {
+          name: 'Stephanie Dias',
+          cpf: '02085196020',
+          phone: '1533294040',
+          email: 'ste.dias@gmail.com',
+          cep: '23825080',
+          state: 'RJ',
+          city: 'Itaguaí',
+          street: 'Rua Mario Bastos Filho',
+        },
+        {
+          name: 'Mirtes Souza',
+          cpf: '33764389001',
+          phone: '1530178756',
+          email: 'irma.mirtes@gmail.com',
+          cep: '40420150',
+          state: 'BA',
+          city: 'Salvador',
+          street: '1ª Travessa Clóvis de Almeida Maia',
+        }
+      ]
+      this.saveLocalStorage()
+    }
   }
 
   saveUser(newUser) {
@@ -68,7 +81,7 @@ export class UsersService {
     else {
       this.users[index] = newUser
     }
-    this.updateLocalStorage()
+    this.saveLocalStorage()
   }
 
   deleteUser(user) {
@@ -76,14 +89,11 @@ export class UsersService {
     if (index != -1) {
       this.users.splice(index, 1)
     }
-    this.updateLocalStorage()
+    this.saveLocalStorage()
   }
 
   constructor() {
-    var stg = localStorage.getItem('crud_users')
-    if (stg) {
-      this.users = JSON.parse(stg)
-    }
+    this.loadLocalStorage()
     this.resetCurrUser()
   }
 }
