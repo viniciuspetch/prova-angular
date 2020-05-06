9891662a51c36d366db92378aec2e9ed2a292a1c
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-05-06 19:05:31 -0300

    Removed unused stuff from main component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'teste-helpper'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('teste-helpper');
-  });
 });
